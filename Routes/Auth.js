1b0d6d8b026b3b04a3f359bf4ac0c4439d32b046
SJSL01 <120094702+SJSL01@users.noreply.github.com>
2022-12-08 21:17:16 +0530

    Error Message Fix!!!!

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -47,7 +47,7 @@ router.post('/login',
 
 router.post("/signup",
 
-    body("username").isAlphanumeric().isLength({ min: 4, max: 10 }).withMessage("Username must be between 4 to 10 characters."),
+    body("username").isAlphanumeric().withMessage("No special characters allowed in username").isLength({ min: 4, max: 10 }).withMessage("Username must be between 4 to 10 characters."),
     body("email").isEmail().withMessage("Please enter a valid Email"),
     body("password").isLength({ min: 6 }).withMessage("Password must be greater than 6 characters")
 
@@ -55,8 +55,8 @@ router.post("/signup",
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            console.log(errors);
-            return res.status(400).json({ errors: errors.array() });
+            console.log(errors.array()[0].msg);
+            return res.status(400).json({ error: errors.array()[0].msg });
         }
 
         try {
